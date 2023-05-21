df02955032657a62192b775cf75ebed746516212
Cat Core <arthur.koalas@free.fr>
2023-05-21 22:25:23 +0200

    Initial Fabrication of workspace
    Fix unimined setup
    
    
    ugh ok weird
    
    
    Update build.gradle
    Update build.gradle
    Update gradle.properties
    Update build.gradle
    Update build.gradle
    Update gradle.properties

--- src/main/java/org/dimdev/rift/Rift.java
+++ src/main/java/org/dimdev/rift/Rift.java
@@ -3,7 +3,6 @@ package org.dimdev.rift;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.dimdev.riftloader.listener.InitializationListener;
-import org.spongepowered.asm.launch.MixinBootstrap;
 import org.spongepowered.asm.mixin.Mixins;
 
 public class Rift implements InitializationListener {
@@ -12,7 +11,6 @@ public class Rift implements InitializationListener {
     public void onInitialization() {
         LOGGER.info("Loading Rift!");
 
-        MixinBootstrap.init();
         Mixins.addConfiguration("mixins.rift.core.json");
         Mixins.addConfiguration("mixins.rift.hooks.json");
     }
